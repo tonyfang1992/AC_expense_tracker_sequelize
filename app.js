34ba785e583e26ff4ae60a3dc1ea033c98a8379b
tonyfang <q710370@gmail.com>
2020-01-05 09:53:24 +0800

    Add home and user routers

--- app.js
+++ app.js
@@ -18,33 +18,12 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+app.use('/users', require('./routes/user'))
 app.get('/', (req, res) => {
   res.send('success!')
 })
 
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+
 app.listen(port, () => {
   console.log(`the web is running on http://localhost:${port}`)
 })
